06a2ed1917efb0a0ba6995dc0c21bdeaa54f2d70
danielBRTanimacao <danieltenorio2046@gmail.com>
2026-06-25 12:33:20 -0300

    feat created docker compose first DB mongo DB add schema and update properties

--- src/main/java/pinstack_api/services/impls/PinServiceImpl.java
+++ src/main/java/pinstack_api/services/impls/PinServiceImpl.java
@@ -36,6 +36,8 @@ public class PinServiceImpl implements PinService{
         entity.setDescription(data.description());
         entity.setImageUrl(data.imageUrl());
 
+        repository.save(entity);
+
         return ResponsePinDTO.fromEntity(entity);
     }
 
