ef37a45fc28f1a87864d1e96a527e46052f10605
danielBRTanimacao <danieltenorio2046@gmail.com>
2026-07-04 10:14:12 -0300

    feat creating new controll

--- src/main/java/pinstack_api/controllers/impls/AuthGraphQLControllerImpl.java
+++ src/main/java/pinstack_api/controllers/impls/AuthGraphQLControllerImpl.java
@@ -1,21 +1,30 @@
 package pinstack_api.controllers.impls;
 
+import org.springframework.stereotype.Controller;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import pinstack_api.DTOs.auth.AuthResponseDTO;
 import pinstack_api.DTOs.auth.LoginAuthDTO;
 import pinstack_api.DTOs.auth.RequestAuthDTO;
 import pinstack_api.DTOs.auth.ResponseJwtDTO;
 import pinstack_api.controllers.AuthGraphQLController;
+import pinstack_api.services.AuthService;
 
-
+@Controller
+@RequiredArgsConstructor
+@Slf4j
 public class AuthGraphQLControllerImpl implements AuthGraphQLController {
+
+    private final AuthService service;
 
     @Override
     public AuthResponseDTO register(RequestAuthDTO data) {
-        throw new UnsupportedOperationException("Unimplemented method 'register'");
+        return service.register(data);
     }
 
     @Override
     public ResponseJwtDTO login(LoginAuthDTO data) {
-        throw new UnsupportedOperationException("Unimplemented method 'login'");
+        return service.login(data);
     }
 }
