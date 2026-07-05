bb9e10ea3a9416dc8beb431d12d22fcbf91473d8
danielBRTanimacao <danieltenorio2046@gmail.com>
2026-07-05 08:24:57 -0300

    feat add location os URLS

--- src/main/java/pinstack_api/config/CorsConfig.java
+++ src/main/java/pinstack_api/config/CorsConfig.java
@@ -1,27 +1,26 @@
 package pinstack_api.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
+
 import java.util.List;
 
 @Configuration
 public class CorsConfig {
 
+    @Value("${app.cors.allowed-origins}")
+    private List<String> allowedOrigins;
+
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
 
-        configuration.setAllowedOrigins(List.of(
-            "http://127.0.0.1:5500",
-            "http://localhost:5500",
-            "http://localhost:3000",
-            "http://localhost:5173"
-        ));
-
+        configuration.setAllowedOrigins(allowedOrigins);
         configuration.setAllowedMethods(List.of("GET", "POST", "OPTIONS", "PUT", "DELETE"));
         configuration.setAllowedHeaders(List.of(
             "Authorization", 
