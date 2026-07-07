bd681e49134175e5c1968d52095fc8dc96466ca0
danielBRTanimacao <danieltenorio2046@gmail.com>
2026-07-07 12:46:12 -0300

    feat updating email service add logs and updating description properties

--- src/main/java/pinstack_api/utils/components/EmailSender.java
+++ src/main/java/pinstack_api/utils/components/EmailSender.java
@@ -1,23 +1,37 @@
 package pinstack_api.utils.components;
 
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.mail.MailException;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.stereotype.Component;
 
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 
 @Component
+@Slf4j
 @RequiredArgsConstructor
 public class EmailSender {
-        
+    
     private final JavaMailSender mailSender;
 
+    @Value("${spring.mail.username}")
+    private String fromEmail;
+
     public void sendVerificationEmail(String email, String verificationCode) {
-        SimpleMailMessage message = new SimpleMailMessage(); 
-        message.setFrom("noreply@baeldung.com");
-        message.setTo(email); 
-        message.setSubject("Verify your email"); 
+        log.info("Sending verification email to {}", email);
+        SimpleMailMessage message = new SimpleMailMessage();
+        message.setFrom(fromEmail);
+        message.setTo(email);
+        message.setSubject("Varificando email");
         message.setText("Copie esse codigo e cole: " + verificationCode);
-        mailSender.send(message);
+        try {
+            mailSender.send(message);
+            log.info("Verification email sent to {}", email);
+        } catch (MailException e) {
+            log.error("Failed to send verification email to {}", email, e);
+            throw e;
+        }
     }
 }
